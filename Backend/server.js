70ed4119ee316b76e6ea44aa4e0e9cea48bcb895
Gaurav Moghe <gauravmoghe0709@gmail.com>
2025-12-30 00:48:47 +0530

    Fix casing in directory names for consistency in frontend and backend paths

--- Backend/server.js
+++ Backend/server.js
@@ -11,11 +11,11 @@ app.listen(3000,()=>{
 const path = require("path");
 
 // serve frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "../frontend/dist/index.html")
+    path.join(__dirname, "../Frontend/dist/index.html")
   );
 });
 
