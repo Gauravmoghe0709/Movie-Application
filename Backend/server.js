4dd4cdd29d13cafa5a85050bb9ca71c96f81e6ff
Gaurav Moghe <gauravmoghe0709@gmail.com>
2025-12-30 12:31:54 +0530

    chages

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,7 @@
 require("dotenv").config();
-const path = require("path");
-const express = require("express");
 
 const app = require("./src/app");
 const connecttodb = require("./src/database/movie.database");
-
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-app.use((req, res) => {
-  res.sendFile(
-    path.join(__dirname, "../Frontend/dist/index.html")
-  );
-});
 
 connecttodb();
 
